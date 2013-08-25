a75d0b811b68990acc0d4a66ab2d9f7329b6fb68
Misam Saki <info@misam.ir>
2013-08-25 20:02:22 +0430

    Upload include files

---
+++ inc/theme-options.js
@@ -0,0 +1,30 @@
+/**
+ * Theme options script
+ * Package: Dinky
+ * Since: Dinky 1.0
+ * Theme URI: http://wordpress.org/themes/dinky/
+ * Github repository URI: http://github.com/misamplus/dinky-template/
+ * More information: http://en.misam.ir/projects/dinky-template/
+ * License: GNU General Public License v3 or later
+ * Copyright (C) 2013  Misam Saki, misam.ir
+ * Author: Misam Saki
+ * Website: http://en.misam.ir/
+ * Email: misamplus@gmail.com
+ * Twitter: @misamplus
+*/
+window.load = options_load();
+function options_load() {
+}
+function sidebar_display_click() {
+	fullmain_nosidebar_display();
+}
+function fullmain_nosidebar_display() {
+	var checked_sidebar_display = document.getElementById("sidebar-display").checked;
+	if (checked_sidebar_display) {
+		document.getElementById("fullmain-nosidebar").style.display = "none";
+		document.getElementById("fullmain-nosidebar-title").style.display = "none";
+	} else {
+		document.getElementById("fullmain-nosidebar").style.display = "block";
+		document.getElementById("fullmain-nosidebar-title").style.display = "block";
+	}
+}
