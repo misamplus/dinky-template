6bd99b5adcf21110fe4f1a70e6dab492ee64af59
Misam Saki <misamplus@gmail.com>
2013-08-25 23:51:10 +0430

    Create page-no_sidebar_full_main.php
    Add page-no_sidebar_full_main

---
+++ page-no_sidebar_full_main.php
@@ -0,0 +1,34 @@
+<?php
+if ( !defined('ABSPATH')) exit;
+/**
+ * Template Name: No Sidebar - Full Main
+ *
+ * The template for displaying pages with 'No Sidebar - Full Main' template.
+ * 
+ * @package Dinky
+ * @version 1.3
+ * @since Dinky 1.0
+ * @license GNU General Public License v3 or later
+ * @copyright (C) 2013  Misam Saki, misam.ir
+ * @author Misam Saki
+ * @website http://en.misam.ir/
+ * @email: misamplus@gmail.com
+ * @twitter: @misamplus
+ */
+?>
+<?php get_header(); ?>
+		<div id="content" class="container" role="main">
+			<div id="main" class="center full">
+				<?php get_sidebar('up_main'); ?>
+				<?php if ( have_posts() ) : ?>
+					<?php while ( have_posts() ) : the_post(); ?>
+						<?php get_template_part( 'content', 'page' ); ?>
+						<?php comments_template( '', true ); ?>
+					<?php endwhile; ?>
+					<?php dinky_pages_nav(); ?>
+				<?php else : ?>
+					<?php get_template_part( 'content', 'none' ); ?>
+				<?php endif;?>
+			</div>
+		</div>
+<?php get_footer(); ?>
